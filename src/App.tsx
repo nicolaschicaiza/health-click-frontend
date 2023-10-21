73b46d264337a39d089e7398357c9615749de781
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-10-21 12:43:57 -0500

    :white_check_mark: (NICO) Creado componente home y navegacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Suspense, lazy } from 'react'
+
+const HomePage = lazy(() => import('./pages/HomePage'));
 
 function App() {
 
   return (
-    <>
-      App
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Suspense fallback={<div>Loading...</div>}><HomePage /></Suspense>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
